4c151d85f9eec310f9d14957280080e550d6cb88
adithya-naik <jatothadithyanaik@gmail.com>
2025-05-17 21:27:58 +0530

    created a docker file

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Order matter kartha hai commands ka !!! ...
+# each step is called layer
+
+# we will use baseImage
+FROM node:20-alpine3.18
+# set working directory
+WORKDIR /app
+# copy these 2 files
+COPY package.json .
+COPY package-lock.json .
+# installing dependencies
+# keep .dockerignore file to avoid node_modules
+RUN npm install
+# copy rest of the code
+COPY . .
+# commands to start the application
+CMD [ "npm" ,"dev" ]
---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express"
+
+const app = express();
+const PORT = 3000
+
+app.get("/", (req, res) => {
+  return res.send("Hello World!")
+})
+
+
+app.get("/products", (req, res) => {
+  return res.json([
+    { id: 1, name: "Trimmer", price: 376 },
+    { id: 12, name: "Bottle", price: 357 },
+    { id: 14, name: "Box", price: 5676 },
+    { id: 1546, name: "Paste", price: 656 }
+  ])
+})
+
+app.listen(PORT, () => {
+  console.log("n\n\nServer is runnning .... \n\n\n")
+})
