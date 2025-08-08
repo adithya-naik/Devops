192f5154708babe1228b1d005881150cfc641f1c
adithya-naik <jatothadithyanaik@gmail.com>
2025-08-08 14:33:19 +0530

    adding bad-code.py

---
+++ bad_code.py
@@ -0,0 +1,11 @@
+def my_function(  a,  b): # too many spaces inside parentheses (E201)
+    print("Sum is:",a+b)  # missing space after comma (E231)
+
+# x=  10   # too many spaces (E221)
+# y =20    # missing space after assignment (E231)
+# print(  x+y ) # extra spaces inside parentheses (E201)
+
+# def unused_function():
+#     pass  # unused function (F401 if imported, but here will be fine)
+
+my_function( x, y)
