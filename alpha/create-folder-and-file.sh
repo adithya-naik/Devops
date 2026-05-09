5871bbde6fbd34ab89006c99e10ae60a7eca8105
adithya-naik <jatothadithyanaik@gmail.com>
2026-05-09 21:31:28 +0530

    add:create-folder-and-file

---
+++ alpha/create-folder-and-file.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Creates the folder and file
+
+mkdir fileFolder
+cd fileFolder
+touch file1.txt file2.txt
+chmod 777 file1.txt
+chmod 777 file2.txt
--- alpha/pull_code_from_current_branch.sh
+++ alpha/pull_code_from_current_branch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# djf
+echo "Run using sh filename.sh or ./filename.sh"
+echo "before running give the executable permissions with (chmod root/owner group others and 777 in binary)"
+echo "-------------------------------------------------------------------------------------------------------------"
 echo "Hi Adithya!"
 echo "You are on :"
 echo "$(git branch)"
